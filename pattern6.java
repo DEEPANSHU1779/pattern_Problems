94c6768f83deb19c0873f966db5875144492ea47
DEEPANSHU1779 <76111537+DEEPANSHU1779@users.noreply.github.com>
2022-01-22 17:56:41 +0530

    Update pattern6.java

--- pattern6.java
+++ pattern6.java
@@ -2,6 +2,16 @@ package patterns;
 
 import java.util.Scanner;
 
+// *
+// **
+// ***
+// ****
+// *****
+// ****
+// ***
+// **
+// *
+
 public class pattern6 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
