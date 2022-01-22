6570cb0d7887ef123c6edf02809b62a752870f48
DEEPANSHU1779 <76111537+DEEPANSHU1779@users.noreply.github.com>
2022-01-22 18:22:30 +0530

    Update pattern10.java

--- pattern10.java
+++ pattern10.java
@@ -2,6 +2,13 @@ package patterns;
 
 import java.util.Scanner;
 
+//      1
+//    2 2 2
+//   3 3 3 3 3
+//  4 4 4 4 4 4 4
+// 5 5 5 5 5 5 5 5 5
+
+
 public class pattern10 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
