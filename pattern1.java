5976d4a2acc8b85868db2e58236a3e2bf22247bf
DEEPANSHU1779 <76111537+DEEPANSHU1779@users.noreply.github.com>
2022-01-22 17:52:22 +0530

    Update pattern1.java

--- pattern1.java
+++ pattern1.java
@@ -1,6 +1,12 @@
 package patterns;
 import java.util.Scanner;
 
+//  *****
+//  *****
+//  *****
+//  *****
+//  *****
+ 
 public class pattern1 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
